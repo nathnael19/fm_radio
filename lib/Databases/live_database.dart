08428a4d45612588bd04133356e9dc9c64539b2c
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2025-07-04 14:04:44 +0300

    creating a local database

---
+++ lib/Databases/live_database.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+List wheathers = [
+  ["አዲስ አበባ", Icons.sunny, "15 / 24", Colors.amber],
+  ["ሃዋሳ", Icons.sunny_snowing, "12 / 24", Colors.blue],
+  ["ድሬደዋ", Icons.sunny, "20 / 24", Colors.amber],
+  ["ባህርዳር", Icons.cloud, "12 / 24", Colors.blueGrey],
+  ["ጀማ", Icons.sunny, "30 / 26", Colors.blue],
+  ["ጎንደር", Icons.sunny, "12 / 24", Colors.blue],
+];
+
+List currency = [
+  ["assets/images/usa.png", "USD", "United States Dollar", 133.13],
+  ["assets/images/uk.png", "GBP", "United States Dollar", 176.71],
+  ["assets/images/europe.png", "EUR", "Euro", 151.30],
+  ["assets/images/safrica.png", "ZAR", "South African Rand", 7.07],
+  ["assets/images/arab.png", "AED", "Arab Emirates Diriham", 36.20],
+];
+
+List todaysProgram = [
+  ["assets/images/girl2.png", "ታዲያስ አዲስ", "06:00 PM"],
+  ["assets/images/girl5.png", "ማለዳ", "04:00 PM"],
+  ["assets/images/man.png", "ማለዳ", "12:00 AM"],
+];
+List recentPrograms = [
+  ["assets/images/girl3.png", "የህይወት ታሪክ", "የተለየ ነው"],
+  ["assets/images/man2.png", "የህይወት ታሪክ", "አስተያየት ነው"],
+  ["assets/images/girl4.png", "የህይወት ታሪክ", "ሙድ ነው"],
+  ["assets/images/girl3.png", "የህይወት ታሪክ", "የተለየ ነው"],
+  ["assets/images/man2.png", "የህይወት ታሪክ", "አስተያየት ነው"],
+  ["assets/images/girl4.png", "የህይወት ታሪክ", "ሙድ ነው"],
+  ["assets/images/girl3.png", "የህይወት ታሪክ", "የተለየ ነው"],
+  ["assets/images/man2.png", "የህይወት ታሪክ", "አስተያየት ነው"],
+  ["assets/images/girl4.png", "የህይወት ታሪክ", "ሙድ ነው"],
+];
