a2623961cb390cd2d3970a117da179ccd95f3c26
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2025-07-04 14:10:39 +0300

    creating a side container icons for live page

---
+++ lib/Home/components/side_container_icons.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class SideContainerIcon extends StatelessWidget {
+  final int number;
+  final IconData icon;
+  final VoidCallback onTap;
+  const SideContainerIcon({
+    super.key,
+    required this.number,
+    required this.icon,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        IconButton(
+          onPressed: onTap,
+          icon: Icon(icon, size: 24, color: Colors.white),
+        ),
+        Text(
+          number.toString(),
+          style: TextStyle(
+            fontSize: 12,
+            color: Colors.white,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ],
+    );
+  }
+}
