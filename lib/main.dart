511216f2fb68ae17d1e8ca71d8cf3cfa214fb40e
Muller1616 <mulugetaabeje16@gmail.com>
2025-07-21 08:57:10 +0300

    fix the error

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:ethio_fm_radio/bottom_navigation.dart';
 import 'package:ethio_fm_radio/cubit/notification/notification_cubit.dart';
-import 'package:ethio_fm_radio/my_page_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
