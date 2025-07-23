e8ce01efbfe1e297816f91146ae9b4070e87810f
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2025-07-23 15:30:57 +0300

    using form validation for signup page

--- lib/Screens/Auth/create_account.dart
+++ lib/Screens/Auth/create_account.dart
@@ -25,9 +25,15 @@ class _SignUpPageState extends State<CreactAccountPage> {
   final TextEditingController confirmPasswordController =
       TextEditingController();
 
+  final String fullname_error = "Enter a valid full name";
+  final String email_error = "Enter a valid email";
+  final String phone_error =
+      "Phone Number cannot be empty of greater than or less than 10 character";
+  String pass_error = "Password must be greater than 8 characters";
   @override
   Widget build(BuildContext context) {
     final local = AppLocalizations.of(context)!;
+    final formKey = GlobalKey<FormState>();
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
@@ -40,83 +46,138 @@ class _SignUpPageState extends State<CreactAccountPage> {
               SizedBox(height: 27.h),
               Padding(
                 padding: EdgeInsets.symmetric(horizontal: 16.w),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    //Page title
-                    Text(
-                      local.signup_page_title,
-                      style: TextStyle(
-                        color: Color(0xff1A0101),
-                        fontSize: 36.sp,
-                        fontWeight: FontWeight.w700,
-                        fontFamily: "Poppins",
+                child: Form(
+                  key: formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      //Page title
+                      Text(
+                        local.signup_page_title,
+                        style: TextStyle(
+                          color: Color(0xff1A0101),
+                          fontSize: 36.sp,
+                          fontWeight: FontWeight.w700,
+                          fontFamily: "Poppins",
+                        ),
                       ),
-                    ),
 
-                    const SizedBox(height: 16),
+                      const SizedBox(height: 16),
 
-                    // //Full Name
-                    // MyTextField(
-                    //     hint: local.full_name, controller: nameController),
+                      //Full Name
+                      MyTextField(
+                          validator: (value) {
+                            if (value!.isEmpty ||
+                                !RegExp(r'^[a-z A-Z]').hasMatch(value)) {
+                              return fullname_error;
+                            } else {
+                              return null;
+                            }
+                          },
+                          hint: local.full_name,
+                          controller: nameController),
 
-                    // const SizedBox(height: 16),
+                      const SizedBox(height: 16),
 
-                    // // Email
-                    // MyTextField(hint: local.email, controller: emailController),
+                      // Email
+                      MyTextField(
+                          validator: (value) {
+                            if (value!.isEmpty ||
+                                !RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w]{2,4}')
+                                    .hasMatch(value)) {
+                              return email_error;
+                            } else {
+                              return null;
+                            }
+                          },
+                          hint: local.email,
+                          controller: emailController),
 
-                    // const SizedBox(height: 16),
+                      const SizedBox(height: 16),
 
-                    // //Phone Number
-                    // MyTextField(
-                    //   hint: local.phone_number,
-                    //   controller: phoneController,
-                    // ),
-                    // const SizedBox(height: 16),
+                      //Phone Number
+                      MyTextField(
+                        validator: (value) {
+                          if (value!.isEmpty ||
+                              !RegExp(r'^[0-9]').hasMatch(value) ||
+                              value.length != 10) {
+                            return phone_error;
+                          } else {
+                            return null;
+                          }
+                        },
+                        hint: local.phone_number,
+                        controller: phoneController,
+                      ),
+                      const SizedBox(height: 16),
+//pass
+                      MyTextField(
+                        validator: (value) {
+                          if (value!.isEmpty || value.length < 8) {
+                            return pass_error;
+                          } else {
+                            return null;
+                          }
+                        },
+                        hint: local.password,
+                        controller: passwordController,
+                      ),
 
-                    // MyTextField(
-                    //   hint: local.password,
-                    //   controller: passwordController,
-                    // ),
+                      const SizedBox(height: 16),
+//confirm pass
+                      MyTextField(
+                        validator: (value) {
+                          if (value!.isEmpty || value.length < 8) {
+                            return pass_error;
+                          } else {
+                            return null;
+                          }
+                        },
+                        hint: local.confirm_pass,
+                        controller: confirmPasswordController,
+                      ),
+                      const SizedBox(height: 16),
 
-                    // const SizedBox(height: 16),
+                      LoginContainer(
+                        title: local.create_acc,
+                        onTap: () {
+                          if (formKey.currentState!.validate()) {
+                            if (passwordController.text !=
+                                confirmPasswordController.text) {
+                              setState(() {
+                                pass_error = "Passwords doesn't match";
+                              });
+                            } else {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => MyBottomNavigation(),
+                                ),
+                              );
+                            }
+                          }
+                        },
+                      ),
+                      // Account Button
+                      const SizedBox(height: 20),
 
-                    // MyTextField(
-                    //   hint: local.confirm_pass,
-                    //   controller: confirmPasswordController,
-                    // ),
-                    // const SizedBox(height: 16),
+                      MyDivider(),
 
-                    LoginContainer(
-                      title: local.create_acc,
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const MyBottomNavigation(),
-                          ),
-                        );
-                      },
-                    ),
-                    // Account Button
-                    const SizedBox(height: 20),
-
-                    MyDivider(),
-
-                    // Already have account link
-                    TextContainer(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const SigninPage(),
-                          ),
-                        );
-                      },
-                      leftText: local.signup_page_question,
-                      rightText: local.signin_text,
-                    ),
-                  ],
+                      // Already have account link
+                      TextContainer(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const SigninPage(),
+                            ),
+                          );
+                        },
+                        leftText: local.signup_page_question,
+                        rightText: local.signin_text,
+                      ),
+                    ],
+                  ),
                 ),
               ),
               const SizedBox(height: 30),
