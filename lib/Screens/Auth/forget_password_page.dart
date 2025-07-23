08cb923cc8228d2df3c99ea97934a9b3bd179aff
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2025-07-23 15:38:20 +0300

    applying form validation to a forgot page

--- lib/Screens/Auth/forget_password_page.dart
+++ lib/Screens/Auth/forget_password_page.dart
@@ -19,10 +19,13 @@ class ForgotPasswordPage extends StatefulWidget {
 class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
   final TextEditingController phoneController =
       TextEditingController(); //Controller for phone number
+  final String phone_error =
+      "Phone Number cannot be empty of greater than or less than 10 character";
 
   @override
   Widget build(BuildContext context) {
     final local = AppLocalizations.of(context)!;
+    final formKey = GlobalKey<FormState>();
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
@@ -36,77 +39,91 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
               SizedBox(height: 27.h),
 
               // Title
-              // Padding(
-              //   padding: EdgeInsets.symmetric(horizontal: 16.w),
-              //   child: Column(
-              //     crossAxisAlignment: CrossAxisAlignment.start,
-              //     children: [
-              //       Text(
-              //         local.forgot_page_title,
-              //         style: TextStyle(
-              //           color: Color(0xff1A0101),
-              //           fontSize: 36.sp,
-              //           fontWeight: FontWeight.w700,
-              //           fontFamily: "Poppins",
-              //         ),
-              //       ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.w),
+                child: Form(
+                  key: formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        local.forgot_page_title,
+                        style: TextStyle(
+                          color: Color(0xff1A0101),
+                          fontSize: 36.sp,
+                          fontWeight: FontWeight.w700,
+                          fontFamily: "Poppins",
+                        ),
+                      ),
 
-              //       SizedBox(height: 16.h),
+                      SizedBox(height: 16.h),
 
-              //       //Subtitle
-              //       Text(
-              //         local.forgot_page_desc,
-              //         style: TextStyle(
-              //           fontSize: 16.sp,
-              //           fontWeight: FontWeight.w500,
-              //           color: Color(0xff001420),
-              //           fontFamily: "Poppins",
-              //         ),
-              //       ),
+                      //Subtitle
+                      Text(
+                        local.forgot_page_desc,
+                        style: TextStyle(
+                          fontSize: 16.sp,
+                          fontWeight: FontWeight.w500,
+                          color: Color(0xff001420),
+                          fontFamily: "Poppins",
+                        ),
+                      ),
 
-              //       SizedBox(height: 16.h),
+                      SizedBox(height: 16.h),
 
-              //       // Phone number field
-              //       MyTextField(
-              //         hint: local.password,
-              //         controller: phoneController,
-              //       ),
+                      // Phone number field
+                      MyTextField(
+                        validator: (value) {
+                          if (value!.isEmpty ||
+                              !RegExp(r'^[0-9]').hasMatch(value) ||
+                              value.length != 10) {
+                            return phone_error;
+                          } else {
+                            return null;
+                          }
+                        },
+                        hint: local.phone_number,
+                        controller: phoneController,
+                      ),
 
-              //       SizedBox(height: 16.h),
+                      SizedBox(height: 16.h),
 
-              //       // Send code button
-              //       LoginContainer(
-              //         title: local.send_code,
-              //         onTap: () {
-              //           Navigator.push(
-              //             context,
-              //             MaterialPageRoute(
-              //               builder: (context) => const EnterCodePage(),
-              //             ),
-              //           );
-              //         },
-              //       ),
+                      // Send code button
+                      LoginContainer(
+                        title: local.send_code,
+                        onTap: () {
+                          if (formKey.currentState!.validate()) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const EnterCodePage(),
+                              ),
+                            );
+                          }
+                        },
+                      ),
 
-              //       SizedBox(height: 24.h),
-              //       MyDivider(),
-              //       SizedBox(height: 24.h),
+                      SizedBox(height: 24.h),
+                      MyDivider(),
+                      SizedBox(height: 24.h),
 
-              //       // Back to Sign In link
-              //       TextContainer(
-              //         onTap: () {
-              //           Navigator.push(
-              //             context,
-              //             MaterialPageRoute(
-              //               builder: (context) => const SigninPage(),
-              //             ),
-              //           );
-              //         },
-              //         leftText: local.signup_page_question,
-              //         rightText: local.signin_text,
-              //       ),
-              //     ],
-              //   ),
-              // ),
+                      // Back to Sign In link
+                      TextContainer(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const SigninPage(),
+                            ),
+                          );
+                        },
+                        leftText: local.signup_page_question,
+                        rightText: local.signin_text,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
         ),
--- lib/Screens/Auth/reset_page.dart
+++ lib/Screens/Auth/reset_page.dart
@@ -1,192 +1,192 @@
-// import 'package:ethio_fm_radio/Screens/Auth/new_password_page.dart';
-// import 'package:ethio_fm_radio/Screens/Auth/signin_page.dart';
-// import 'package:ethio_fm_radio/l10n/app_localizations.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_screenutil/flutter_screenutil.dart';
-// import 'package:flutter_svg/flutter_svg.dart'; // ✅ import flutter_svg
+import 'package:ethio_fm_radio/Screens/Auth/new_password_page.dart';
+import 'package:ethio_fm_radio/Screens/Auth/signin_page.dart';
+import 'package:ethio_fm_radio/l10n/app_localizations.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart'; // ✅ import flutter_svg
 
-// class EnterCodePage extends StatefulWidget {
-//   const EnterCodePage({super.key});
+class EnterCodePage extends StatefulWidget {
+  const EnterCodePage({super.key});
 
-//   @override
-//   State<EnterCodePage> createState() => _EnterCodePageState();
-// }
+  @override
+  State<EnterCodePage> createState() => _EnterCodePageState();
+}
 
-// class _EnterCodePageState extends State<EnterCodePage> {
-//   final List<TextEditingController> _controllers = List.generate(
-//     6,
-//     (index) => TextEditingController(),
-//   );
-//   final List<FocusNode> _focusNodes = List.generate(6, (index) => FocusNode());
+class _EnterCodePageState extends State<EnterCodePage> {
+  final List<TextEditingController> _controllers = List.generate(
+    6,
+    (index) => TextEditingController(),
+  );
+  final List<FocusNode> _focusNodes = List.generate(6, (index) => FocusNode());
 
-//   @override
-//   void dispose() {
-//     for (final controller in _controllers) {
-//       controller.dispose();
-//     }
-//     for (final node in _focusNodes) {
-//       node.dispose();
-//     }
-//     super.dispose();
-//   }
+  @override
+  void dispose() {
+    for (final controller in _controllers) {
+      controller.dispose();
+    }
+    for (final node in _focusNodes) {
+      node.dispose();
+    }
+    super.dispose();
+  }
 
-//   void _onChanged(String value, int index) {
-//     if (value.isNotEmpty && index < 5) {
-//       _focusNodes[index + 1].requestFocus();
-//     } else if (value.isEmpty && index > 0) {
-//       _focusNodes[index - 1].requestFocus();
-//     }
-//   }
+  void _onChanged(String value, int index) {
+    if (value.isNotEmpty && index < 5) {
+      _focusNodes[index + 1].requestFocus();
+    } else if (value.isEmpty && index > 0) {
+      _focusNodes[index - 1].requestFocus();
+    }
+  }
 
-//   void _onConfirm() {
-//     String code = _controllers.map((e) => e.text).join();
-//     if (code.length == 6) {
-//       Navigator.push(
-//         context,
-//         MaterialPageRoute(builder: (context) => const SetNewPasswordPage()),
-//       );
-//     } else {
-//       ScaffoldMessenger.of(context).showSnackBar(
-//         const SnackBar(content: Text("Please enter all 6 digits")),
-//       );
-//     }
-//   }
+  void _onConfirm() {
+    String code = _controllers.map((e) => e.text).join();
+    if (code.length == 6) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const SetNewPasswordPage()),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Please enter all 6 digits")),
+      );
+    }
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     final local = AppLocalizations.of(context)!;
-//     return Scaffold(
-//       body: Container(
-//         width: double.infinity,
-//         decoration: const BoxDecoration(
-//           image: DecorationImage(
-//             image: AssetImage("assets/images/wavecurve.jpg"),
-//             fit: BoxFit.cover,
-//           ),
-//         ),
-//         child: Stack(
-//           children: [
-//             Column(
-//               mainAxisAlignment: MainAxisAlignment.end,
-//               children: [
-//                 Padding(
-//                   padding: EdgeInsets.symmetric(horizontal: 25.w),
-//                   child: Column(
-//                     mainAxisSize: MainAxisSize.min,
-//                     crossAxisAlignment: CrossAxisAlignment.start,
-//                     children: [
-//                       // Title
-//                       Text(
-//                         local.reset_page_title,
-//                         style: TextStyle(
-//                           fontSize: 28,
-//                           fontWeight: FontWeight.bold,
-//                           color: Colors.black,
-//                         ),
-//                       ),
-//                       const SizedBox(height: 10),
+  @override
+  Widget build(BuildContext context) {
+    final local = AppLocalizations.of(context)!;
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/wavecurve.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Stack(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25.w),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // Title
+                      Text(
+                        local.reset_page_title,
+                        style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
 
-//                       // Subtitle
-//                       Text(
-//                         "${local.reset_page_desc} +251 91 234 567",
-//                         textAlign: TextAlign.center,
-//                         style: TextStyle(
-//                             fontSize: 16, fontWeight: FontWeight.w500),
-//                       ),
-//                       const SizedBox(height: 30),
+                      // Subtitle
+                      Text(
+                        "${local.reset_page_desc} +251 91 234 567",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w500),
+                      ),
+                      const SizedBox(height: 30),
 
-//                       // Functional OTP Inputs
-//                       Row(
-//                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//                         children: List.generate(6, (index) {
-//                           return SizedBox(
-//                             width: 40,
-//                             height: 50,
-//                             child: TextField(
-//                               controller: _controllers[index],
-//                               focusNode: _focusNodes[index],
-//                               textAlign: TextAlign.center,
-//                               maxLength: 1,
-//                               keyboardType: TextInputType.number,
-//                               style: const TextStyle(fontSize: 22),
-//                               decoration: InputDecoration(
-//                                 counterText: '',
-//                                 hintText: '*',
-//                                 hintStyle: const TextStyle(
-//                                   color: Colors.grey,
-//                                   fontSize: 22,
-//                                 ),
-//                                 enabledBorder: const UnderlineInputBorder(
-//                                   borderSide: BorderSide(color: Colors.grey),
-//                                 ),
-//                                 focusedBorder: const UnderlineInputBorder(
-//                                   borderSide: BorderSide(
-//                                     color: Color(0xFF80011F),
-//                                     width: 2,
-//                                   ),
-//                                 ),
-//                               ),
-//                               onChanged: (value) => _onChanged(value, index),
-//                             ),
-//                           );
-//                         }),
-//                       ),
-//                       const SizedBox(height: 30),
+                      // Functional OTP Inputs
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: List.generate(6, (index) {
+                          return SizedBox(
+                            width: 40,
+                            height: 50,
+                            child: TextField(
+                              controller: _controllers[index],
+                              focusNode: _focusNodes[index],
+                              textAlign: TextAlign.center,
+                              maxLength: 1,
+                              keyboardType: TextInputType.number,
+                              style: const TextStyle(fontSize: 22),
+                              decoration: InputDecoration(
+                                counterText: '',
+                                hintText: '*',
+                                hintStyle: const TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 22,
+                                ),
+                                enabledBorder: const UnderlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.grey),
+                                ),
+                                focusedBorder: const UnderlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Color(0xFF80011F),
+                                    width: 2,
+                                  ),
+                                ),
+                              ),
+                              onChanged: (value) => _onChanged(value, index),
+                            ),
+                          );
+                        }),
+                      ),
+                      const SizedBox(height: 30),
 
-//                       // Confirm Button
-//                       SizedBox(
-//                         width: double.infinity,
-//                         height: 50,
-//                         child: ElevatedButton(
-//                           style: ElevatedButton.styleFrom(
-//                             backgroundColor: Color(0xFF80011F),
-//                             shape: RoundedRectangleBorder(
-//                               borderRadius: BorderRadius.circular(30),
-//                             ),
-//                           ),
-//                           // onPressed: () {},
-//                           onPressed: _onConfirm,
-//                           child: Text(
-//                             local.confirm,
-//                             style: TextStyle(fontSize: 18, color: Colors.white),
-//                           ),
-//                         ),
-//                       ),
-//                       const SizedBox(height: 20),
+                      // Confirm Button
+                      SizedBox(
+                        width: double.infinity,
+                        height: 50,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xFF80011F),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                          ),
+                          // onPressed: () {},
+                          onPressed: _onConfirm,
+                          child: Text(
+                            local.confirm,
+                            style: TextStyle(fontSize: 18, color: Colors.white),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
 
-//                       // Resend Code
-//                       GestureDetector(
-//                         onTap: () {
-//                           // Handle resend logic here
-//                         },
-//                         child: Text(
-//                           local.resend_text,
-//                           style: TextStyle(
-//                             fontSize: 16,
-//                             color: Color(0xFF80011F),
-//                             fontWeight: FontWeight.w500,
-//                           ),
-//                         ),
-//                       ),
-//                     ],
-//                   ),
-//                 ),
-//                 SizedBox(
-//                   height: 80.h,
-//                 ),
-//               ],
-//             ),
-//             Positioned(
-//                 left: MediaQuery.of(context).size.width / 2 - 50.w,
-//                 top: 250.h,
-//                 child: SvgPicture.asset("assets/images/sss.svg")),
-//             Positioned(
-//                 left: MediaQuery.of(context).size.width / 2 - 22.w,
-//                 top: 290.h,
-//                 child: SvgPicture.asset("assets/images/checkk.svg")),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+                      // Resend Code
+                      GestureDetector(
+                        onTap: () {
+                          // Handle resend logic here
+                        },
+                        child: Text(
+                          local.resend_text,
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Color(0xFF80011F),
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 80.h,
+                ),
+              ],
+            ),
+            Positioned(
+                left: MediaQuery.of(context).size.width / 2 - 50.w,
+                top: 250.h,
+                child: SvgPicture.asset("assets/images/sss.svg")),
+            Positioned(
+                left: MediaQuery.of(context).size.width / 2 - 22.w,
+                top: 290.h,
+                child: SvgPicture.asset("assets/images/checkk.svg")),
+          ],
+        ),
+      ),
+    );
+  }
+}
