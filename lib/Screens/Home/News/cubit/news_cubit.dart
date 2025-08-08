8ed650f8a94953f7f1420521004bc268861593cc
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2025-08-08 04:36:00 -0400

    making the news data online and fetching it

--- lib/Screens/Home/News/cubit/news_cubit.dart
+++ lib/Screens/Home/News/cubit/news_cubit.dart
@@ -1,26 +1,38 @@
 import 'dart:convert';
 
 import 'package:ethio_fm_radio/Screens/Home/News/model/news_model.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:http/http.dart' as http;
 
 part 'news_state.dart';
 
 class NewsCubit extends Cubit<NewsState> {
   NewsCubit() : super(NewsInitial());
 
+  // Replace with your actual Bin ID and secret key from jsonbin.io or your JSON URL
+  final String binId = '6895b409203a8b52b5e19ae3';
+  final String secretKey =
+      "\$2a\$10\$OdrPUzOSvGmobZmaalIaC.xV3OJsf4kym57057x7siedund/ZQTMC";
+
   Future<void> loadNews() async {
     emit(NewsLoading());
     try {
-      final jsonString =
-          await rootBundle.loadString("assets/data/news_database.json");
-      print(jsonString);
-      if (jsonString.trim().isEmpty) {
-        throw Exception("Json is Empty!!");
+      final url = Uri.parse('https://api.jsonbin.io/v3/b/$binId/latest');
+      final response = await http.get(url, headers: {
+        'X-Master-Key': secretKey,
+      });
+
+      if (response.statusCode != 200) {
+        throw Exception('Failed to load JSON: ${response.statusCode}');
       }
 
-      List<dynamic> jsonData = json.decode(jsonString);
-      final category = jsonData.map((e) => NewsModel.fromJson(e)).toList();
+      final jsonString = response.body;
+      final jsonData = jsonDecode(jsonString);
+      // jsonbin.io wraps your data inside 'record'
+      final List<dynamic> dataList = jsonData['record'];
+
+      final category = dataList.map((e) => NewsModel.fromJson(e)).toList();
+
       emit(NewsLoaded(category: category));
     } catch (e) {
       emit(NewsError(message: e.toString()));
